6179299b5ab363bf6c5048ec5784c483a624d785
Chris Darch <comradenapcal@aol.com>
2020-04-04 17:32:18 -0700

    resources: Init commit.

---
+++ build.bat
@@ -0,0 +1,18 @@
+@echo off
+title Toontown Journey Resource Compiler
+echo Welcome to the Toontown Journey Resource Compiler
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf phase_3.mf phase_3
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf phase_3.5.mf phase_3.5
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf phase_4.mf phase_4
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf phase_5.mf phase_5
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf phase_5.5.mf phase_5.5
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf phase_6.mf phase_6
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf phase_7.mf phase_7
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf phase_8.mf phase_8
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf phase_9.mf phase_9
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf phase_10.mf phase_10
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf phase_11.mf phase_11
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf phase_12.mf phase_12
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf phase_13.mf phase_13
+C:/Panda3D-1.11.0-x64/bin/multify.exe -cf TL_Cookies.mf TL_Cookies
+pause
